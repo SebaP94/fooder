adcddd9024dc53417d2fe2deacdd775c4f6f02a2
spaduch <sebastian.paduch@vattenfall.com>
2021-04-09 00:01:46 +0200

    ci: Test ci/cd

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ export const App = () => (
 						Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
 					</Text>
 					<Link color="teal.500" href="https://chakra-ui.com" fontSize="2xl" target="_blank" rel="noopener noreferrer">
-						Deployed from CD
+						Test CD
 					</Link>
 				</VStack>
 			</Grid>
